ad69cfc6e626782353ca4bf878785fb2091852d3
Luke Tarr <lrt22@pitt.edu>
2023-02-05 21:47:18 -0500

    yarn stuff

--- src/components/Pages/Home/Home.tsx
+++ src/components/Pages/Home/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
                 <h1 className="text-3xl mb-4">Welcome to SoloLink!</h1>
                 <h3>
                     SoloLink is a free and open source alternative to services like
-                    Linktree. Here you can host a list useful links seperated by
+                    Linktree. Here you can host a list of useful links seperated by
                     categories. A wishlist from your favorite shopping site? All of your
                     social media profiles? Platforms where people can stream your music?
                     It's all up to you, and completely free forever!
